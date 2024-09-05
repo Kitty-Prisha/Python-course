abde6e48d7604409b1f7d9dc44fb4813f9a65b32
Kitty-Prisha <mileedcy@gmail.com>
2024-09-05 19:40:58 +0530

    first commit

---
+++ Looping ch1/while.py
@@ -0,0 +1,10 @@
+#Print 1 to 10 numbers with the help of while loop
+i=1
+while i<10:
+    print(i)
+    i=i+1
+#printing even numbers with the help of while loops
+i=0
+while i<20:
+    print(i)
+    i=i+2
