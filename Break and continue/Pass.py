212bc6a51289676f2c83334fa53155404d9ca641
Kitty-Prisha <mileedcy@gmail.com>
2024-10-01 16:00:35 +0530

    breakandcontinue

---
+++ Break and continue/Pass.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number"))
+if a%20==0:
+    print("This is divisible by 20")
+elif a%15==0:
+    pass
+elif a%5==0:
+    print("This is divisible by 5")
+elif a%3==0:
+    print("This is divisible by 3")  
+else:
+    print("None")      
