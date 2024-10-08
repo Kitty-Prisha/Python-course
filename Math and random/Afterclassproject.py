cabdbbfa3102b37513ef913a92f7c544dd5cfa2d
Kitty-Prisha <mileedcy@gmail.com>
2024-10-08 17:55:58 +0530

    mathAcp

---
+++ Math and random/Afterclassproject.py
@@ -0,0 +1,7 @@
+import math
+op=float(input("Enter the opposite in the triangle"))
+hy=float(input("Enter the hypotenuse in the triangle"))
+ad=float(input("Enter the adjacent in the triangle"))
+print("The sine is ",math.ceil(op)/math.ceil(hy))
+print("The Cosine is ",math.ceil(ad)/math.ceil(hy))
+print("The Tanget is ",math.ceil(op)/math.ceil(ad))
