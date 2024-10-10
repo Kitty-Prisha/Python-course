f792fc36c13165967e281f8c347695c4d2f0cf18
Kitty-Prisha <mileedcy@gmail.com>
2024-10-10 20:23:40 +0530

    datee

---
+++ Date and time/ACP.py
@@ -0,0 +1,27 @@
+from datetime import date,time,datetime
+import calendar
+m1=1
+y1=2012
+print(calendar.month(y1,m1))
+m2=2
+print(calendar.month(y1,m2))
+m3=3
+print(calendar.month(y1,m3))
+m4=4
+print(calendar.month(y1,m4))
+m5=5
+print(calendar.month(y1,m5))
+m6=6
+print(calendar.month(y1,m6))
+m7=7
+print(calendar.month(y1,m7))
+m8=8
+print(calendar.month(y1,m8))
+m9=9
+print(calendar.month(y1,m9))
+m10=10
+print(calendar.month(y1,m10))
+m11=11
+print(calendar.month(y1,m11))
+m12=12
+print(calendar.month(y1,m12))
