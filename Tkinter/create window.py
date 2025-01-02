38c4e35aa02da403bc49cfc64635487641b6df8a
Kitty-Prisha <mileedcy@gmail.com>
2025-01-02 16:59:47 +0530

    trinket

--- Tkinter/create window.py
+++ Tkinter/create window.py
@@ -6,8 +6,19 @@ lbl=Label(text="Enter your name",fg='white', bg='black',height=3, width=20)
 lbl.pack()
 entry=Entry(fg='white',bg='grey',width=20)
 entry.place(x=150,y=100)
-button=Button(fg='black',bg='grey',text='submit')
-button.pack()
+
+
+name_entry=Entry()
 def display():
-    
+    name=name_entry.get()
+    messgae="Welcome to application!"
+    greet="Hello"+name+"/n"
+    text_box.insert(END,greet)
+    text_box.insert(END,messgae)
+    text_box=Text(height=3)
+    text_box.pack()
+button=Button(fg='black',bg='grey',text='Begin',command=display())
+button.pack()
+
+
 window.mainloop()
