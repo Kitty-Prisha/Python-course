45b608ba6be706cf42a4acfe528b1d67541a818c
Kitty-Prisha <mileedcy@gmail.com>
2025-01-30 16:52:20 +0530

    data

---
+++ Date and time/acp asymptotic.py
@@ -0,0 +1,26 @@
+def printnumber(n):
+    iteration=0
+    print(n)
+    iteration=iteration+1
+    print(iteration)
+printnumber(20)
+#O(n)Time
+def time(n):
+    iteration=0
+    for i in range(1,n+1):
+        print(n)
+        iteration=iteration+1
+    print(iteration)
+time(30) 
+#O(n^2)
+def time2(n):
+    iteration=0
+    for i in range(0,n):
+        for j in range(0,n):
+    
+            print("*",end=" ")
+            iteration=iteration+1
+        print(" ")
+    print(iteration)
+time2(40)            
+
