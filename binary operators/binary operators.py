ba2579c6ed3db7e51d7079fb4a61d2bdfa987ede
Kitty-Prisha <mileedcy@gmail.com>
2025-02-20 16:35:35 +0530

    binary

---
+++ binary operators/binary operators.py
@@ -0,0 +1,13 @@
+num1=10
+num2=9
+print(bin(num1))
+print(bin(num2))
+print(num1&num2)
+print(num1|num2)
+#not
+print(~num1)
+#XOR
+print(num1^num2)
+#left and right shift
+print(num1>>1)
+print(num1<<1)
