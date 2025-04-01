79bf3e901e60c2dd798f1a20641632068b98ad07
Kitty-Prisha <mileedcy@gmail.com>
2025-04-01 16:32:15 +0530

    recursion

--- Recursion/Largestarray.py
+++ Recursion/Largestarray.py
@@ -2,6 +2,6 @@ def arrlargest(a):
     length=len(a)
     if length==1:
         return a[0]
-    return max(a[0],arrlargest[1:])
+    return max(a[0],arrlargest(a[1:]))
 a=[3,45,769,987,27]
 print(arrlargest(a))
